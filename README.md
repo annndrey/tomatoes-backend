b91d6619895b7b9d74105fd4f6831900abe3120c
annndrey <gontchar@gmail.com>
2019-11-28 16:26:39 +0300

    Added Detectron2-based predict.py file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Leaves recognition demo
+# Leaves recognition demo
+
+## Prerequisites
+
+* `pip install 'git+https://github.com/facebookresearch/fvcore'`
+* `pycocotools: pip install cython; pip install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'`
+* git clone https://github.com/facebookresearch/detectron2.git
+* pip install google-colab
--- predict2.py
+++ predict2.py
@@ -18,7 +18,7 @@ from google.colab.patches import cv2_imshow
 # import some common detectron2 utilities
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer,Visualizer_with_labeled_colors
+from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 
 from detectron2.engine import DefaultTrainer
