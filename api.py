1b57d2de73fee23b5e9e93e5f1b2d27729f766cf
annndrey <gontchar@gmail.com>
2020-02-15 20:41:40 +0000

    Rewrote the salad classifier with the FastAPI (Starlette-based) async web framework

--- api.py
+++ api.py
@@ -1,96 +1,89 @@
-#!/usr/bin/env python
-#Ю -*- coding: utf-8 -*-
-
-from functools import wraps
-from flask import Flask, g, make_response, request, current_app
-from flask_restful import Resource, Api, reqparse, abort, marshal_with
-from flask.json import jsonify
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import or_
-from sqlalchemy import func as sql_func
-from flask_marshmallow import Marshmallow
-from flask_httpauth import HTTPBasicAuth
-from flask_cors import CORS, cross_origin
-from flask_restful.utils import cors
-from marshmallow import fields
-from marshmallow_enum import EnumField
-from models import db, User, UserQuery
-
-import os
-import uuid
-
-import click
-import datetime
-import calendar
-from dateutil.relativedelta import relativedelta
-import jwt
-import json
-
-# AI
+import logging
 import torch
 import torchvision
-from torchvision import datasets, models, transforms
-
 import io
-import requests
-from PIL import Image
-import glob
+import numpy as np 
+
+from fastapi import FastAPI
+from fastapi import UploadFile
+from fastapi import File
+from starlette.requests import Request
+from torchvision import datasets, models, transforms
 from collections import OrderedDict
+from PIL import Image
+from imgaug import augmenters as iaa 
 
 
-app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}}, support_credentials=True, methods=['GET', 'POST', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'])
-api = Api(app, prefix="/api/v1")
-auth = HTTPBasicAuth()
-app.config.from_envvar('APPSETTINGS')
-app.config['PROPAGATE_EXCEPTIONS'] = True
-db.init_app(app)
-migrate = Migrate(app, db)
-ma = Marshmallow(app)
+MODEL = "/home/annndrey/cityfarmer/cityf_41cl_s3_35_acc946.md"
+USING_MODEL_NAME = 'densenet169'
+NUM_CLASSES_USED = 41
+RESIZE = 224
 
-# _____________________ AI Section _____________________
+logger = logging.getLogger("api")
+app = FastAPI(redoc_url=None, docs_url=None)
 
-tomat_or_not_path = app.config['TOMAT_OR_NOT_PATH']
-plant_health_or_not_path = app.config['PLANT_HEALTH_OR_NOT_PATH']
-tomat_health_or_not_path = app.config['TOMAT_HEALTH_OR_NOT_PATH']
-using_model_name = app.config['USING_MODEL_NAME']
-num_classes_used = app.config['NUM_CLASSES_USED']
-using_data_transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
 
-modres = ( { 0 : "it's not a tomato", 1 : "it's a tomato" },
-           { 0 : "it's a healthy tomato", 1 : "it's an unhealthy tomato" },
-           { 0 : "it's a healthy plant", 1 : "it's an unhealthy plant" }
-)
+class ImgResizeAndPad:
+    #max_cropped_part - maximum percentage of each side length cropped (with probability ~0.5)
+#max_ratio_change - maximum relative increase of the short side toward square size (with probability 0.5). Set it to 1 to keep the aspect ratio of the img always.
+    def __init__(self, resize=224, max_ratio_change = 1.2):
+        self.max_ratio_change = max_ratio_change
+        self.resize = resize
+        self.pad = iaa.size.PadToFixedSize(width = resize, height = resize, pad_mode='constant', pad_cval=0, position='center')
+    def __call__(self, img):
+        img = np.array(img)
+        (h,w) = img.shape[0:2]
+        keep_h=False
+        M = w
+        m = h
+        if h>w :
+            keep_h=True
+            M = h
+            m = w
+        if self.max_ratio_change > 1:
+            new_m = int(m*self.max_ratio_change*self.resize/M)
+            if new_m>self.resize: new_m=self.resize
+        else:
+            new_m = int(m*self.resize/M)
+        if keep_h:
+            resize_to_square = iaa.Resize({"height": self.resize, "width": new_m})
+        else:
+            resize_to_square = iaa.Resize({"height": new_m, "width": self.resize})
+        img = resize_to_square.augment_image( img )
+        img = self.pad.augment_image( img )
+        return img
 
-aimodels = ()
 
-torch.set_grad_enabled(False)
+only_make_square_transform = transforms.Compose([
+    ImgResizeAndPad(resize=RESIZE),
+    lambda x: Image.fromarray(x),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) ])
 
-def load_pretrained_weights(model, weight_path):
-    """Load pretrianed weights to model
-    Incompatible layers (unmatched in name or size) will be ignored
-    Args:
-    - model (nn.Module): network model, which must not be nn.DataParallel
-    - weight_path (str): path to pretrained weights
-    """
-#    checkpoint = torch.load(weight_path)
-    checkpoint = torch.load(weight_path, map_location='cpu')
+
+def get_model_results(model, results, img):
+    idx_to_class = {v: k for k, v in model.class_to_idx.items()}
+    outputs = model(img)
+    _, preds = torch.max(outputs, 1)
+    detected_img_type = idx_to_class[int(preds)]
+    return detected_img_type
+
+
+
+def load_model(file_name):
+    checkpoint = torch.load(file_name, map_location='cpu')
     if 'state_dict' in checkpoint:
         state_dict = checkpoint['state_dict']
     else:
         state_dict = checkpoint
+    arch = checkpoint['arch']
+    class_to_idx = checkpoint['class_to_idx']
+    num_classes = len(class_to_idx)
+    model = torchvision.models.__dict__[arch](num_classes=num_classes)
     model_dict = model.state_dict()
     new_state_dict = OrderedDict()
     matched_layers, discarded_layers = [], []
     for k, v in state_dict.items():
-        # If the pretrained state_dict was saved as nn.DataParallel,
-        # keys would contain "module.", which should be ignored.
         if k.startswith('module.'):
             k = k[7:]
         if k in model_dict and model_dict[k].size() == v.size():
@@ -100,342 +93,31 @@ def load_pretrained_weights(model, weight_path):
             discarded_layers.append(k)
     model_dict.update(new_state_dict)
     model.load_state_dict(model_dict)
-    if len(matched_layers) == 0:
-        warnings.warn('The pretrained weights "{}" cannot be loaded, please check the key names manually (** ignored and continue **)'.format(weight_path))
-    else:
-        print('Successfully loaded pretrained weights from "{}"'.format(weight_path))
-        if len(discarded_layers) > 0:
-            print("** The following layers are discarded due to unmatched keys or layer size: {}".format(discarded_layers))
+    model.class_to_idx = class_to_idx
 
-            
-@app.before_first_request            
-def loadmodels():
-    print("loading models")
-    tomat_or_not_model = torchvision.models.__dict__[using_model_name](num_classes=num_classes_used)
-    load_pretrained_weights(tomat_or_not_model, tomat_or_not_path)
-    tomat_or_not_model.eval()
-
-    plant_health_or_not_model = torchvision.models.__dict__[using_model_name](num_classes=num_classes_used)
-    load_pretrained_weights(plant_health_or_not_model, plant_health_or_not_path)
-    plant_health_or_not_model.eval()
-
-    tomat_health_or_not_model = torchvision.models.__dict__[using_model_name](num_classes=num_classes_used)
-    load_pretrained_weights(tomat_health_or_not_model, tomat_health_or_not_path)
-    tomat_health_or_not_model.eval()
-    global aimodels
-    aimodels = (tomat_or_not_model, tomat_health_or_not_model, plant_health_or_not_model)
-
-# _____________________ AI Section _____________________
+    return model
 
 
-def token_required(f):  
-    @wraps(f)
-    def _verify(*args, **kwargs):
-        auth_headers = request.headers.get('Authorization', '').split()
+@app.on_event("startup")
+async def startup_event():
+    global aimodel
+    aimodel = ""
+    trained_model = load_model(MODEL)
+    trained_model.eval()
+    aimodel = trained_model
 
-        invalid_msg = {
-            'message': 'Invalid token. Registeration and / or authentication required',
-            'authenticated': False
-        }
-        expired_msg = {
-            'message': 'Expired token. Reauthentication required.',
-            'authenticated': False
-        }
-
-        if len(auth_headers) != 2:
-            return abort(403)
-
-        #try:
-        token = auth_headers[1]
-        data = jwt.decode(token, current_app.config['SECRET_KEY'])
-        user = User.query.filter_by(login=data['sub']).first()
-
-        if not user:
-            abort(404)
-
-        return f(*args, **kwargs)
-
-    return _verify
-
-
-def authenticate(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if not getattr(func, 'authenticated', True):
-            return func(*args, **kwargs)
-
-        acct = basic_authentication()  # custom account lookup function
-
-        if acct:
-            return func(*args, **kwargs)
-
-        abort(401)
-    return wrapper
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
-
-@auth.verify_password
-def verify_password(username_or_token, password):
-    user = User.verify_auth_token(username_or_token)
-    if not user:
-        # try to authenticate with username/password
-        user = User.query.filter_by(login = username_or_token).first()
-        if not user or not user.verify_password(password):
-            return False
-    g.user = user
-    return True
-
-
-@app.route('/api/v1/token', methods=['POST'])
-@cross_origin(supports_credentials=True)
-def get_auth_token_post():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    user = User.query.filter_by(login = username).first()
-    if user:
-        if user.verify_password(password):
-            token = user.generate_auth_token()
-            response = jsonify({ 'token': "%s" % token.decode('utf-8'), "user_id":user.id, "login": user.login, "name": user.name })
-            return response
-    abort(404)
-
-
-@app.route('/api/v1/token', methods=['GET'])
-def get_auth_token():
-    token = g.user.generate_auth_token()
-    return jsonify({ 'token': "%s" % token })
-
-
-# SCHEMAS 
-class UserSchema(ma.ModelSchema):
-    class Meta:
-        model = User
-
-
-class StatsAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
     
-    def options(self, *args, **kwargs):
-        return jsonify([])
+@app.post("/loadimage")
+async def create_upload_file(request: Request):
+    form = await request.form()
+    imagefile = form["imagefile"].filename
+    contents = await form["imagefile"].read()
 
-    @token_required
-    @cross_origin()
-    def get(self, id=None):
-        auth_headers = request.headers.get('Authorization', '').split()
-        token = auth_headers[1]
-        data = jwt.decode(token, current_app.config['SECRET_KEY'])
-        user = User.query.filter_by(login=data['sub']).first()
-        if not user:
-            abort(404)
-
-        return jsonify({})
-
-    @token_required
-    @cross_origin()
-    def post(self):
-        auth_headers = request.headers.get('Authorization', '').split()
-        token = auth_headers[1]
-        udata = jwt.decode(token, current_app.config['SECRET_KEY'])
-        user = User.query.filter_by(login=udata['sub']).first()
-        fpath = os.path.join(current_app.config['FILE_PATH'], user.login)
-        maxqueryage = current_app.config['QUERY_AGE']
-        if not user:
-            return abort(403)
-        index = request.form['index']
-        orig_name = request.form['filename']
-        f = request.files['croppedfile']
-        data = f.read()
-        fsize = len(data)
-        imgext = os.path.splitext(f.filename)[-1]
-        #print(1)
-        if not os.path.exists(fpath):
-            os.makedirs(fpath)
-        #print(2)
-        fuuid = str(uuid.uuid4())
-        fname = fuuid + imgext
-        #print(3)
-        prevquery = db.session.query(UserQuery).filter(UserQuery.orig_name == orig_name).filter(UserQuery.fsize == fsize).filter(UserQuery.user == user).first()
-        if prevquery and prevquery.queryage <= maxqueryage:
-            #print(11)
-            # return existing data without calculating
-            print("SAVED RESULTS")
-            resp = json.loads(prevquery.result)
-        else:
-            print("NEW RESULTS")
-            fullpath = os.path.join(fpath, fname)
-
-            with open(fullpath, 'wb') as outf:
-                outf.write(data)
-            #print(4)
-            # AI Section start
-            img_pil = Image.open(io.BytesIO(data))
-            print(img_pil)
-            #print(5)
-            img_tensor = using_data_transform(img_pil)
-            img_tensor.unsqueeze_(0)
-            img_variable = img_tensor
-            result = {}
-            #print(6)
-            for model in aimodels:
-                outputs = model(img_variable)
-                _, preds = torch.max(outputs, 1)
-                key = f.filename
-                if key in result:
-                    result[key].append( int(preds) )
-                else:
-                    result[key]= [int(preds)]
-             # AI Section ends
-            #print(7)
-            # Plant: Tomato/Not tomato
-            # Status: Health/Unhealthy
-            # if tomato:
-            resdata = result[f.filename]
-            planttype = ""
-            plantstatus = ""
-            #print(8)
-            if  resdata[0] == 0:
-                planttype = "Not tomato"
-                if resdata[2] == 0:
-                    plantstatus = "Healthy"
-                else:
-                    plantstatus = "Unhealthy"
-            else:
-                planttype = "Tomato"
-                if resdata[1] == 0:
-                    plantstatus = "Healthy"
-                else:
-                    plantstatus = "Unhealthy"
-
-            #print(9)
-            resp  = {'planttype': planttype, 'plantstatus': plantstatus, 'index': index, 'filename': f.filename}
-
-            newquery = UserQuery(local_name=fname, orig_name=orig_name, user=user, result=json.dumps(resp), fsize=fsize)
-            db.session.add(newquery)
-            db.session.commit()
-            #print(10)
-        print(jsonify(resp))
-        return jsonify(resp)
-
-
-class UserAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.schema = UserSchema(exclude=['password_hash',])
-        self.m_schema = UserSchema(many=True, exclude=['password_hash',])
-        self.method_decorators = []
-
-        
-    def options(self, *args, **kwargs):
-        return jsonify([])
-        
-    @token_required
-    @cross_origin()
-    def get(self, id=None):
-        if not id:
-            users = db.session.query(User).all()
-            return jsonify(self.m_schema.dump(users).data)
-        else:
-            user = User.query.filter_by(id=id).first()
-            if user:
-                return jsonify(self.schema.dump(user).data), 200
-            else:
-                abort(404)
-
-    @token_required
-    @cross_origin()
-    def patch(self, id):
-        if not request.json:
-            abort(400, message="No data provided")
-            
-        user = db.session.query(User).filter(User.id==id).first()
-        if user:
-            for attr in ['login', 'phone', 'name', 'note', 'is_confirmed', 'confirmed_on', 'password']:
-                val = request.json.get(attr)
-                if attr == 'password' and val:
-                    user.hash_password(val)
-                    
-                elif attr == 'confirmed_on':
-                    val = datetime.datetime.now()
-
-                        
-                if val:
-                    setattr(user, attr, val)
-                
-            db.session.add(user)
-            db.session.commit()
-            return jsonify(self.schema.dump(user).data), 201
-        
-        abort(404, message="Not found")
-
-    @token_required
-    @cross_origin()
-    def post(self):
-        if not request.json:
-            abort(400, message="No data provided")
-        login = request.json.get('login')
-        phone = request.json.get('phone')
-        name = request.json.get('name')
-        password = request.json.get('password')
-        
-        if not(any([login, phone, name])):
-            return abort(400, 'Provide required fields for phone, name or login')
-        
-        prevuser = db.session.query(User).filter(User.login==login).first()
-        if prevuser:
-            abort(409, message='User exists')
-        note = request.json.get('note')
-
-        is_confirmed = request.json.get('is_confirmed')
-        confirmed_on = None
-        if is_confirmed:
-            confirmed_on = datetime.datetime.today()
-
-        newuser = User(login=login, is_confirmed=is_confirmed, confirmed_on=confirmed_on, phone=phone, name=name, note=note)
-        
-        newuser.hash_password(password)
-        db.session.add(newuser)
-        db.session.commit()
-        
-        return jsonify(self.schema.dump(newuser).data), 201
+    img_pil = Image.open(io.BytesIO(contents))
+    img_tensor = only_make_square_transform(img_pil)
+    img_tensor.unsqueeze_(0)
+    img_variable = img_tensor
+    result = {}
+    result = get_model_results(aimodel, result, img_variable)
     
-    def delete(self, id):
-        if not id:
-            abort(404, message="Not found")
-        user = db.session.query(User).filter(User.id==id).first()
-        if user:
-            db.session.delete(user)
-            db.session.commit()
-            return make_response("User deleted", 204)
-        abort(404, message="Not found")
-
-api.add_resource(UserAPI, '/users', '/users/<int:id>', endpoint = 'users')
-api.add_resource(StatsAPI, '/loadimage', endpoint = 'loadimage')
-
-
-@app.cli.command()
-@click.option('--login',  help='user@mail.com')
-@click.option('--password',  help='password')
-@click.option('--name',  help='name')
-@click.option('--phone',  help='phone')
-def adduser(login, password, name, phone):
-    """ Create new user"""
-        
-    newuser = User(login=login, name=name, phone=phone)
-    newuser.hash_password(password)
-    newuser.is_confirmed = True
-    newuser.confirmed_on = datetime.datetime.today()
-    db.session.add(newuser)
-    db.session.commit()
-    print("New user added", newuser)
-
-                
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    return {"filename": imagefile, "length": len(contents), 'objtype': result}
