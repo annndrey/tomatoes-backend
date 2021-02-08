94b9c884d1db54106e64d7cab648f0e8bc4a6ddd
Andrew <gontchar@gmail.com>
2021-02-08 23:51:23 +0300

    fix

--- config_dev.py
+++ config_dev.py
@@ -6,12 +6,8 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 FILE_PATH = "/home/andrey/data/TOMATOES"
 
 # models
-PLANT_OR_NOT_PATH = "/home/anton/fermata/codes/a_leaf_classifier/trained_models/vgg19_plant-non_plant_acc996_060419.md"
-POTATO_FIT_OR_NOT_PATH = "/home/anton/fermata/codes/a_leaf_classifier/trained_models/potat_fitoftor_or_not_no_kuzn_aug1_70-40ep.md"
-#LEAF_OR_NOT_PATH = "/home/anton/fermata/codes/a_leaf_classifier/trained_models/vgg19_leaf-plant_non_leaf_acc993_060419.md"
-#TOMAT_OR_NOT_PATH = "/home/anton/fermata/codes/a_leaf_classifier/trained_models/tomat_or_not_69_seed2_acc993.md"
-#PLANT_HEALTH_OR_NOT_PATH = "/home/anton/fermata/codes/a_leaf_classifier/trained_models/plant_health_or_not_16_seed2_acc994.md"
-#TOMAT_HEALTH_OR_NOT_PATH = "/home/anton/fermata/codes/a_leaf_classifier/trained_models/plant_health_or_not_16_seed2_acc994.md"
+PLANT_OR_NOT_PATH = "path_to_model.md"
+POTATO_FIT_OR_NOT_PATH = "path+to-model.md"
 
 
 USING_MODEL_NAME = 'vgg19'
